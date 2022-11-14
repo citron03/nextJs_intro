bfb6f0edb8c6458d2374b2d323aaf30253350112
Citron <72535475+citron03@users.noreply.github.com>
2022-11-14 17:19:21 +0900

    style 적용

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,22 +1,49 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+// import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link
+      {/* <Link
         href="/"
-        style={{ color: router.pathname === "/" ? "red" : "black" }}
+        className={`${styles.link} ${
+          router.pathname === "/" ? styles.active : ""
+        }`}
       >
         Home
       </Link>
       <Link
         href="/about"
-        style={{ color: router.pathname === "/about" ? "red" : "black" }}
+        className={[
+          styles.link,
+          router.pathname === "/about" ? styles.active : "",
+        ].join(" ")}
+      >
+        About
+      </Link> */}
+      <Link href="/" className={router.pathname === "/" ? "active" : ""}>
+        Home
+      </Link>
+      <Link
+        href="/about"
+        className={router.pathname === "/about" ? "active" : ""}
       >
         About
       </Link>
+      <style jsx global>{`
+        nav {
+          background-color: tomato;
+        }
+        // CSS 적용 X, global 옵션 넣으면 적용 됨
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: white;
+        }
+      `}</style>
     </nav>
   );
 }
